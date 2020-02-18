5cff2cbf0ca0e03dfebea21ae753a20977b3f954
Zezombye <Zezombye@mei.is.bae>
2020-02-18 20:45:57 +0100

    test

--- anonymize.sh
+++ anonymize.sh
@@ -2,18 +2,11 @@
 
 git filter-branch --env-filter '
 
-OLD_EMAIL="$1"
 CORRECT_NAME="Zezombye"
-CORRECT_EMAIL=""
+CORRECT_EMAIL="Zezombye@owo.opy"
 
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
-    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-fi
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 ' --tag-name-filter cat -- --branches --tags
