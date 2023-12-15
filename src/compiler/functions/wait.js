db86d0d0a1a295a3f058e77323452b3dee288585
Zezombye <Zezombye@users.noreply.github.com>
2023-12-15 03:47:40 +0100

    Update wait.js

--- src/compiler/functions/wait.js
+++ src/compiler/functions/wait.js
@@ -19,7 +19,7 @@
 
 astParsingFunctions.wait = function(content) {
 
-    if (content.args[1].name === "__number__" && content.args[1].args[0].numvalue === 9999) {
+    if (content.args[0].name === "__number__" && content.args[0].args[0].numvalue === 9999) {
         warn("w_wait_9999", "wait(9999) is not enough because a custom game can last up to 16200 seconds. Use Math.INFINITY or 99999.")
     }
     
