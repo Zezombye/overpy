5e25ad327ff56a0b530a3e027ace316c1fa62d4d
Cactus Puppy <puppycactus@gmail.com>
2024-06-02 21:57:18 -0700

    style: standardize formatting of types definition file

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,19 +1,19 @@
 export const enum ow_languages {
-  enUS="en-US",
-  esMX="es-MX",
-  frFR="fr-FR",
-  jaJP="ja-JP",
-  ptBR="pt-BR",
-  trTR="tr-TR",
-  zhCN="zh-CN",
-  deDE="de-DE",
-  esES="es-ES",
-  itIT="it-IT",
-  koKR="ko-KR",
-  plPL="pl-PL",
-  ruRU="ru-RU",
-  thTH="th-TH",
-  zhTW="zh-TW",
+  enUS = "en-US",
+  esMX = "es-MX",
+  frFR = "fr-FR",
+  jaJP = "ja-JP",
+  ptBR = "pt-BR",
+  trTR = "tr-TR",
+  zhCN = "zh-CN",
+  deDE = "de-DE",
+  esES = "es-ES",
+  itIT = "it-IT",
+  koKR = "ko-KR",
+  plPL = "pl-PL",
+  ruRU = "ru-RU",
+  thTH = "th-TH",
+  zhTW = "zh-TW",
 }
 type OWLanguage = `${ow_languages}`;
 
@@ -41,8 +41,8 @@ export type Value = {
   isConstant?: boolean,
   canBePutInBoolean?: boolean,
 } & {
-  [language in OWLanguage]?: string;
-}
+    [language in OWLanguage]?: string;
+  }
 
 export type Argument = {
   name: string,
@@ -70,51 +70,51 @@ export type Map = {
   onlyInOw1?: boolean,
   isWorkshopMap?: boolean
 } & {
-  [language in OWLanguage]?: string
-}
+    [language in OWLanguage]?: string
+  }
 
 export enum Overwatch2Heroes {
-    ana = "ana",
-    ashe = "ashe",
-    baptiste = "baptiste",
-    bastion = "bastion",
-    brigitte = "brigitte",
-    cassidy = "cassidy",
-    doomfist = "doomfist",
-    dva = "dva",
-    echo = "echo",
-    genji = "genji",
-    hanzo = "hanzo",
-    illari = "illari",
-    junkerQueen = "junkerQueen",
-    junkrat = "junkrat",
-    kiriko = "kiriko",
-    lifeweaver = "lifeweaver",
-    lucio = "lucio",
-    mauga = "mauga",
-    mei = "mei",
-    mercy = "mercy",
-    moira = "moira",
-    orisa = "orisa",
-    pharah = "pharah",
-    ramattra = "ramattra",
-    reaper = "reaper",
-    reinhardt = "reinhardt",
-    roadhog = "roadhog",
-    sigma = "sigma",
-    sojourn = "sojourn",
-    soldier76 = "soldier",
-    sombra = "sombra",
-    symmetra = "symmetra",
-    torbjorn = "torbjorn",
-    tracer = "tracer",
-    venture = "venture",
-    widowmaker = "widowmaker",
-    winston = "winston",
-     // Because Zezombye used this name and now we're stuck with it
-    hammond = "hammond",
-    zarya = "zarya",
-    zenyatta = "zenyatta",
+  ana = "ana",
+  ashe = "ashe",
+  baptiste = "baptiste",
+  bastion = "bastion",
+  brigitte = "brigitte",
+  cassidy = "cassidy",
+  doomfist = "doomfist",
+  dva = "dva",
+  echo = "echo",
+  genji = "genji",
+  hanzo = "hanzo",
+  illari = "illari",
+  junkerQueen = "junkerQueen",
+  junkrat = "junkrat",
+  kiriko = "kiriko",
+  lifeweaver = "lifeweaver",
+  lucio = "lucio",
+  mauga = "mauga",
+  mei = "mei",
+  mercy = "mercy",
+  moira = "moira",
+  orisa = "orisa",
+  pharah = "pharah",
+  ramattra = "ramattra",
+  reaper = "reaper",
+  reinhardt = "reinhardt",
+  roadhog = "roadhog",
+  sigma = "sigma",
+  sojourn = "sojourn",
+  soldier76 = "soldier",
+  sombra = "sombra",
+  symmetra = "symmetra",
+  torbjorn = "torbjorn",
+  tracer = "tracer",
+  venture = "venture",
+  widowmaker = "widowmaker",
+  winston = "winston",
+  // Because Zezombye used this name and now we're stuck with it
+  hammond = "hammond",
+  zarya = "zarya",
+  zenyatta = "zenyatta",
 }
 
 export type Hero = Overwatch2Heroes;
@@ -129,8 +129,8 @@ export type HeroData = {
   ability3?: LocalizableString,
   ultimate: LocalizableString
 } & {
-  [language in OWLanguage]?: string
-}
+    [language in OWLanguage]?: string
+  }
 
 export type ReplaceableData = {
   canReplace0ByFalse?: boolean,
@@ -141,7 +141,7 @@ export type ReplaceableData = {
 
 export type ReturnType = string | { Array: string } | { Direction: [string, string, string] }
 
-export type Type = string[] | string | {[key: string]: string | Type[]};
+export type Type = string[] | string | { [key: string]: string | Type[] };
 
 export type Token = {
   text: string,
@@ -169,7 +169,7 @@ export type ScriptFileStackMember = BaseFileStackMember & {
 export type MacroFileStackMember = BaseFileStackMember & {
   staticMember: false,
   remainingChars: number,
-	callNbChars: number,
+  callNbChars: number,
   callCols: number,
   callLines: number
 }
